42882f9c831d441836b4aa52e81a12c3f5a358f1
Li Yi <liyi_math@pku.edu.cn>
2017-11-12 08:52:12 -0800

    add test script (very rough)

---
+++ test.py
@@ -0,0 +1,22 @@
+from pycoq.core import CoqWorker
+from pycoq.config import *
+from pycoq.logger import logger
+from logging import INFO
+
+
+set_serapi_addr("/home/liyi/projects/coq-serapi/sertop.native")
+set_raw_send_receive_debug(True, False)
+# set_coq_addr("/home/liyi/projects/coq-v8.7")
+# logger.setLevel(INFO)
+
+w = CoqWorker()
+
+with open("db/trivial.v") as fin:
+    cache = ""
+    for line in fin:
+        # it is very import to remove all the endl chars, it may lead to weird behavior in serapi
+        cache += line.replace("\n", " ")
+        if cache.strip().endswith("."):
+            sid = w.add_and_execute_raw(cache)
+            goals = w.query_goals(sid)
+            cache = ""
