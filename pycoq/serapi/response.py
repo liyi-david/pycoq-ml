e3cf836568ca0c66bbad5d3e64a46cce956b5288
Li Yi <liyi_math@pku.edu.cn>
2017-11-12 08:52:49 -0800

    restructured serapi answers through sub-classes

--- pycoq/serapi/response.py
+++ pycoq/serapi/response.py
@@ -1,5 +1,6 @@
 from pycoq.serapi.coqobj import coq_obj_parse
 
+
 def parse_response(resp_tuple):
     if len(resp_tuple) == 0:
         return None
@@ -7,62 +8,73 @@ def parse_response(resp_tuple):
     if resp_tuple[0] == "Feedback":
         return SerFeedBack(resp_tuple)
     elif resp_tuple[0] == "Answer":
-        return SerAnswer(resp_tuple)
+        return SerAnswer.parse(resp_tuple)
     else:
         raise Exception("unknown response type %s" % str(resp_tuple[0]))
 
 
 class SerFeedBack:
-    def __init__(self, resp_tuple):
+    def __init__(self, _):
         pass
 
 
 class SerAnswer:
-    def __init__(self, resp_tuple):
-        assert len(resp_tuple) == 3
+    type = "SerAnswer"
 
-        self.answered_tag = resp_tuple[1]
-        self.answered_kind = parse_answer_kind(resp_tuple[2])
-
-
-def parse_answer_kind(kind_tuple):
-    if not isinstance(kind_tuple, list):
-        if kind_tuple == 'Ack':
-            return SerAnswerAck()
-        elif kind_tuple == 'Completed':
-            return SerAnswerCompleted()
-    else:
-        if kind_tuple[0] == 'Added':
-            return SerAnswerAdded(kind_tuple)
-        elif kind_tuple[0] == 'CoqExn':
-            return SerAnswerException(kind_tuple)
-        elif kind_tuple[0] == 'ObjList':
-            return SerAnswerObjList(kind_tuple)
-
-    raise Exception("unhandled answer_kind %s" % (kind_tuple))
-
-
-class SerAnswerAck:
-    pass
-
-
-class SerAnswerCompleted:
-    pass
-
-
-class SerAnswerAdded:
-    def __init__(self, tuple):
-        self.stateid = int(tuple[1])
-        # TODO Loc.t && ...
-
-
-class SerAnswerException:
-    def __init__(self, tuple):
+    def __init__(self, answered_tag, _):
         pass
+        self.answered_tag = answered_tag
+
+    @classmethod
+    def parse(cls, answer_tuple):
+        for sub_cls in SerAnswer.__subclasses__():
+            answer_kind_name = answer_tuple[2]
+            if isinstance(answer_kind_name, list):
+                answer_kind_name = answer_kind_name[0]
+
+            if sub_cls.type == answer_kind_name:
+                return sub_cls(answer_tuple[1], answer_tuple[2])
+
+        raise Exception("unhandled answer_kind %s" % (answer_tuple[2]))
 
 
-class SerAnswerObjList:
-    def __init__(self, tuple):
-        self.objs = []
-        for obj in tuple[1]:
-            self.objs.append(coq_obj_parse(obj))
+class SerAnswerAck(SerAnswer):
+    type = "Ack"
+
+    def __init__(self, answered_tag, answer_kind):
+        SerAnswer.__init__(self, answered_tag, answer_kind)
+
+
+class SerAnswerCompleted(SerAnswer):
+    type = "Completed"
+
+    def __init__(self, answered_tag, answer_kind):
+        SerAnswer.__init__(self, answered_tag, answer_kind)
+
+
+class SerAnswerAdded(SerAnswer):
+    type = "Added"
+
+    def __init__(self, answered_tag, answer_kind):
+        self.state_id = int(answer_kind[1])
+        # TODO Loc.t && ...
+        SerAnswer.__init__(self, answered_tag, answer_kind)
+
+
+class SerAnswerException(SerAnswer):
+    type = "CoqExn"
+
+    def __init__(self, answered_tag, answer_kind):
+        # TODO
+        SerAnswer.__init__(self, answered_tag, answer_kind)
+
+
+class SerAnswerObjList(SerAnswer):
+    type = "ObjList"
+
+    def __init__(self, answered_tag, answer_kind):
+        self.objects = []
+        for obj in answer_kind[1]:
+            self.objects.append(coq_obj_parse(obj))
+
+        SerAnswer.__init__(self, answered_tag, answer_kind)
